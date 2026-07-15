da98921e70cffb549554bd92f8ffcc9ce4df8627
Adam Fine <afine@Adams-MacBook-Pro.local>
2026-07-14 23:01:02 -0400

    module 2

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+hbs.registerHelper('eq', function(a, b) {
+  return a === b;
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
